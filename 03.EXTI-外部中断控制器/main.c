053d0df6b474be97cc686734f2f867dd1c4c4389
十七阿哥 <72618021+Xiaolei0412@users.noreply.github.com>
2021-01-18 10:51:14 +0800

    #include "stm32f10x.h"  "bsp_led.h" "bsp_exti.h"
    #include "stm32f10x.h"  "bsp_led.h"  "bsp_exti.h"

---
+++ 03.EXTI-外部中断控制器/main.c
@@ -0,0 +1,21 @@
+#include "stm32f10x.h"   // 相当于51单片机中的  #include <reg51.h>
+#include "bsp_led.h"
+#include "bsp_exti.h"
+void Delay( uint32_t count )
+{
+	for(; count!=0; count--);
+}
+
+int main(void)
+{
+	// 来到这里的时候，系统的时钟已经被配置成72M。
+	LED_GPIO_Config();
+	EXTI_KEY_Config();
+	
+	while(1)
+	{
+		
+	}
+}
+
+
